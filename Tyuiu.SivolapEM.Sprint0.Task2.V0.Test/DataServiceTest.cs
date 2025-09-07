430b0f4346e786a46be691652bdd8159bccf82f3
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-07 23:52:55 +0500

    Add Task2

---
+++ Tyuiu.SivolapEM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SivolapEM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SivolapEM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет Егор", res);
+        }
+    }
+}
---
+++ Tyuiu.SivolapEM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.SivolapEM.Sprint0.Task2.V0.Lib
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Егор");
+        Console.ReadKey();
+    }
+}
