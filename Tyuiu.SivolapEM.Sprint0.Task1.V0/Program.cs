5e9f6d9bceada0b61352862ebf302cbbd28a2c91
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-07 23:01:42 +0500

    add Task1

---
+++ Tyuiu.SivolapEM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string name = "Егор";
+        int age = 18;
+
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+        Console.ReadKey();
+    }
+}
