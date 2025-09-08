a860afa9f26e02768c9d633bc5fe40028d22d9d3
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-08 15:48:53 +0500

    add Task6

---
+++ Tyuiu.SivolapEM.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SivolapEM.Sprint0.Task6.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+        Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+        Console.WriteLine("Проиведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+
+        Console.ReadKey();
+    }
+}
